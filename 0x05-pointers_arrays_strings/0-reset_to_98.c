1b32fc9204b3f1e78e1157aa37bdbc7758378b86
muazusuleotu <muazsuleotu@gmail.com>
2022-09-19 06:59:32 -0700

    Update Pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and update the value to 98
+ *
+ *@n - nothing
+ *
+ * Return: void
+ */
+
+void reset_to_98()
+{
+  *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Project on arrays an pointers
